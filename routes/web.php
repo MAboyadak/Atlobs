55b3a2b0d421fe12832b305092a8e8b36ad30629
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-02-21 19:59:47 +0200

    terms and conditions  & Bank Account

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\ProfileController;
-<<<<<<< HEAD
 use App\Http\Controllers\AboutUs ;
 use App\Http\Controllers\TermsAndConditions;
 use App\Http\Controllers\BankAccount ;
-=======
-use App\Http\Controllers\AboutUs;
->>>>>>> 5d03426b3cba0135424f652ee8d58a6e8b70fd8c
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,7 +21,6 @@ Route::resource('categories', CategoryController::class);
 
 Route::get('services', [ServicesController::class, 'index'])->name('services.index');
 Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
-<<<<<<< HEAD
 // About Us
 Route::get('aboutus',[AboutUs::class , 'index'])->name('aboutus.index');
 // termsandconditions
@@ -33,9 +28,4 @@ Route::get('termsandconditions',[TermsAndConditions::class , 'index'])->name('te
 //Bank Account
 
 Route::get('bankAccount',[BankAccount::class , 'index'])->name('bankAcount.index');
-=======
-Route::get('profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-
-Route::get('aboutus', [AboutUs::class, 'index'])->name('aboutus.index');
->>>>>>> 5d03426b3cba0135424f652ee8d58a6e8b70fd8c
 // Route::get('{cat}/services',[ServicesController::class,'categoryServices'])->name('category.services');
