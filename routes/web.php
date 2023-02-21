853f25e2f43d2d0cff66eb2d497ab32163d8036b
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-02-21 19:50:43 +0200

    terms and conditions  & Bank Account

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AboutUs ;
+use App\Http\Controllers\TermsAndConditions;
+use App\Http\Controllers\BankAccount ;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +21,11 @@ Route::resource('categories', CategoryController::class);
 
 Route::get('services', [ServicesController::class, 'index'])->name('services.index');
 Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
-
+// About Us
 Route::get('aboutus',[AboutUs::class , 'index'])->name('aboutus.index');
+// termsandconditions
+Route::get('termsandconditions',[TermsAndConditions::class , 'index'])->name('terms.index');
+//Bank Account
+
+Route::get('bankAccount',[BankAccount::class , 'index'])->name('bankAcount.index');
 // Route::get('{cat}/services',[ServicesController::class,'categoryServices'])->name('category.services');
