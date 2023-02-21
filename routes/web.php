0560dc8a77aafbf1a675af121a2c02ecbca60d11
Mohamed <m.abdelazem0202@gmail.com>
2023-02-21 05:02:36 +0200

    creating profile page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,6 @@ Auth::routes();
 
 Route::resource('categories', CategoryController::class);
 
-Route::get('services',[ServicesController::class,'index'])->name('services.index');
+Route::get('services', [ServicesController::class, 'index'])->name('services.index');
+Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
 // Route::get('{cat}/services',[ServicesController::class,'categoryServices'])->name('category.services');
-
