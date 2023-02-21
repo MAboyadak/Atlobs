c0295db243b270e8b05dc272370a7af3f9da093f
hendshaaaban123 <hendshaaban123z@gamil.com>
2023-02-21 20:19:34 +0200

    comment

---
+++ resources/views/blogs/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')
+@section('content')
+<div class="container-fluid row">
+    <div class="d-flex flex-row-reverse flex-wrap align-items-center justify-content-start">
+       <h1 class="text-end">المدونه</h1>
+    </div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+    <div class="card">
+    <a class="navbar-brand" href="{{route("blogs.blog")}}"><img class="rounded-top" style="width:100%" src="{{asset('images/p2.jpg')}}" alt="">
+        <div class="card-body">
+    <p class="text-dark">3:14  فى <span>10/7/2022</span></p>
+    <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+
+    <p class="text-dark text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص, لقد تم توليد هذا النص من مولدالنص العربى
+        هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+
+    </div>
+    </a>
+    </div>
+</div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+
+    <div class="card">
+        <a class="navbar-brand" href="#"><img class="rounded-top" style="width:100%" src="{{asset('images/p3.jpg')}}" alt="">
+            <div class="card-body">
+        <p class="text-dark">3:14 فى <span>10/7/2022</span></p>
+        <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+        <p class="text-dark  text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى
+            هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+        </div>
+        </a>
+        </div>
+</div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+    <div class="card">
+        <a class="navbar-brand" href="#"><img class="rounded-top" style="width:100%" src="{{asset('images/p4.jpg')}}" alt="">
+            <div class="card-body">
+        <p class="text-dark">3:14 فى <span>10/7/2022</span></p>
+        <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+        <p class="text-dark text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى
+            هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+        </div>
+        </a>
+        </div>
+</div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+    <div class="card">
+        <a class="navbar-brand" href="#"><img class="rounded-top" style="width:100%" src="{{asset('images/p2.jpg')}}" alt="">
+            <div class="card-body">
+        <p class="text-dark">3:14 فى <span>10/7/2022</span></p>
+        <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+        <p class="text-dark  text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى
+            هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+        </div>
+        </a>
+        </div>
+</div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+    <div class="card">
+        <a class="navbar-brand" href="#"><img class="rounded-top" style="width:100%" src="{{asset('images/p3.jpg')}}" alt="">
+            <div class="card-body">
+        <p class="text-dark">3:14 فى <span>10/7/2022</span></p>
+        <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+        <p class="text-dark text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى
+            هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+        </div>
+        </a>
+        </div>
+</div>
+<div class="container col-12 col-md-6 col-lg-4 my-5">
+    <div class="card">
+        <a class="navbar-brand" href="#"><img class="rounded-top" style="width:100%" src="{{asset('images/p4.jpg')}}" alt="">
+            <div class="card-body">
+        <p class="text-dark">3:14 فى <span>10/7/2022</span></p>
+        <p style="direction: rtl; color:blue;" class="fw-bold">سياره ديجيتال</p>
+        <p class="text-dark  text-wrap" style="direction: rtl; "> هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى
+            هذا النص هو مثال لنص يمكن يستبدل فى نفس النص,لقد تم توليد هذا النص من مولدالنص العربى </p>
+        </div>
+        </a>
+        </div>
+</div>
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,6 +17,8 @@ Auth::routes();
 
 Route::resource('categories', CategoryController::class);
 
+Route::get('blogs', [BlogController::class, 'index'])->name('blogs.index');
+Route::get('blog', [BlogController::class, 'blog'])->name('blogs.blog');
 Route::get('services', [ServicesController::class, 'index'])->name('services.index');
 Route::get('profile', [ProfileController::class, 'index'])->name('profile.index');
 // Route::get('{cat}/services',[ServicesController::class,'categoryServices'])->name('category.services');
