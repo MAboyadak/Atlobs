5083b65ca632b379fcb025265678ea43b6f9c38a
Mohamed <m.abdelazem0202@gmail.com>
2023-02-26 23:29:42 +0200

    rtl change password

--- resources/views/profile/change_password.blade.php
+++ resources/views/profile/change_password.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
     <div class="orderBg1 p-4">
-        <div style="max-width:500px" class="container card text-end p-4 " id="editProfile">
+        <div style="max-width:500px" class="container card text-end p-4 pb-5" id="editProfile">
             <form action="" method="post">
                 <p class="fs-3 fw-bold contact-txt-color-1">تغيير كلمةالمرور</p>
-                <div class=" row flex-row-reverse g-3">
+                <div class=" row g-3">
                     <div class="col-10">
                         <label for="currentPassword" class="form-label">كلمة المرور الحالية</label>
                         <input type="password" class="form-control bg-white shadow-none" id="currentPassword">
@@ -13,7 +13,7 @@
                         <label for="newPasswd" class="form-label">كلمة المرور الجديدة</label>
                         <input type="password" class="form-control bg-white shadow-none" id="newPasswd">
                     </div>
-                    <div class="col-10">
+                    <div class="col-10 mb-5">
                         <label for="confirmNewPasswd" class="form-label">تأكيد كلمة المرور الجديدة</label>
                         <input type="password" class="form-control bg-white shadow-none" id="confirmNewPasswd">
                     </div>
