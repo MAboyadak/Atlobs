859526c97b85616f8039a47ad4d3a8955ecfbfd4
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-03-01 13:00:41 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::view('admin', 'admin.dashboard');
 
 //admain route 
 
-Route::middleware(['auth', 'user-access:super-admin'])->group(function () {
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
   
-    Route::get('/super-admin/dashboard', [HomeController::class, 'superAdminDashboard'])->name('super.admin.dashboard');
+    Route::get('/admin/home', [HomeController::class, 'AdminHome'])->name('admin.home');
 });
